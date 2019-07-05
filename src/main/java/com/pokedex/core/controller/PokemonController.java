acc68d804e613f8ef47499bfb7446c9939fb3a4f
Byron <Byron@BYRON>
2019-07-05 11:43:07 -0500

    Servicio de listas pokemones

--- src/main/java/com/pokedex/core/controller/PokemonController.java
+++ src/main/java/com/pokedex/core/controller/PokemonController.java
@@ -1,5 +1,27 @@
 package com.pokedex.core.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pokedex.core.entity.PokemonEntity;
+import com.pokedex.core.service.PokemonService;
+
+@RestController
+@RequestMapping("/pokemon")
+@Lazy
 public class PokemonController {
+
+	@Autowired
+	public PokemonService pokemonService;
+
+	@GetMapping("/listByState")
+	public List<PokemonEntity> listByState() {
+		return this.pokemonService.list();
+	}
 
 }

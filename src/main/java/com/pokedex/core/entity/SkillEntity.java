b7f33a404ea42b0bf5f71fa5abfcc5d05feba04a
fpsilva <fpsilva@BPUIO01610094L.uio.bpichincha.com>
2019-07-05 11:34:25 -0500

    Ignore test

--- src/main/java/com/pokedex/core/entity/SkillEntity.java
+++ src/main/java/com/pokedex/core/entity/SkillEntity.java
@@ -1,5 +1,79 @@
 package com.pokedex.core.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "pokemonskills")
 public class SkillEntity {
+
+	@Id
+	@Column(name = "skiid")
+	private Integer pokemonSkillId;
+
+	@Column(name = "skiname")
+	private String skillName;
+
+	@Column(name = "skitype")
+	private String skillType;
+
+	@Column(name = "skidescription")
+	private String skillDescription;
+
+	/**
+	 * @return the pokemonSkillId
+	 */
+	public Integer getPokemonSkillId() {
+		return pokemonSkillId;
+	}
+
+	/**
+	 * @param pokemonSkillId the pokemonSkillId to set
+	 */
+	public void setPokemonSkillId(Integer pokemonSkillId) {
+		this.pokemonSkillId = pokemonSkillId;
+	}
+
+	/**
+	 * @return the skillName
+	 */
+	public String getSkillName() {
+		return skillName;
+	}
+
+	/**
+	 * @param skillName the skillName to set
+	 */
+	public void setSkillName(String skillName) {
+		this.skillName = skillName;
+	}
+
+	/**
+	 * @return the skillType
+	 */
+	public String getSkillType() {
+		return skillType;
+	}
+
+	/**
+	 * @param skillType the skillType to set
+	 */
+	public void setSkillType(String skillType) {
+		this.skillType = skillType;
+	}
+
+	/**
+	 * @return the skillDescription
+	 */
+	public String getSkillDescription() {
+		return skillDescription;
+	}
+
+	/**
+	 * @param skillDescription the skillDescription to set
+	 */
+	public void setSkillDescription(String skillDescription) {
+		this.skillDescription = skillDescription;
+	}
 
 }

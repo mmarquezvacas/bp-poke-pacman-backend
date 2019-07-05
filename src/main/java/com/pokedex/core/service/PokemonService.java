8d7cdc8d0c6cc94556d9fb7d56668e8c52732a7c
fpsilva <fpsilva@BPUIO01610094L.uio.bpichincha.com>
2019-07-05 15:24:54 -0500

    Micro actualizar el estado pokemon

--- src/main/java/com/pokedex/core/service/PokemonService.java
+++ src/main/java/com/pokedex/core/service/PokemonService.java
@@ -2,9 +2,13 @@ package com.pokedex.core.service;
 
 import java.util.List;
 
+import org.springframework.data.repository.query.Param;
+
 import com.pokedex.core.entity.PokemonEntity;
 
 public interface PokemonService {
 
 	List<PokemonEntity> list();
+
+	void changePokemonStatus(@Param("pokid") Integer pokid, @Param("pokState") String pokState);
 }
--- src/main/java/com/pokedex/core/service/impl/PokemonServiceImpl.java
+++ src/main/java/com/pokedex/core/service/impl/PokemonServiceImpl.java
@@ -7,15 +7,14 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import com.pokedex.core.entity.PokemonEntity;
 import com.pokedex.core.repository.PokemonRepository;
 import com.pokedex.core.service.PokemonService;
 
 @Service
 @Transactional
-public class PokemonServiceImpl implements PokemonService{
-	
+public class PokemonServiceImpl implements PokemonService {
+
 	@Autowired
 	public PokemonRepository pokemonRepository;
 
@@ -29,6 +28,10 @@ public class PokemonServiceImpl implements PokemonService{
 
 		return this.pokemonRepository.findAll();
 	}
-	
+
+	@Override
+	public void changePokemonStatus(Integer pokid, String pokState) {
+		this.pokemonRepository.changePokemonStatus(pokid, pokState);
+	}
 
 }
